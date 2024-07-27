0cd42b81bd1973da2d1f593a20a96a1a207b77eb
martinjohnm <johnmmartin74@gmail.com>
2024-07-27 23:15:45 +0530

    merge sort in ts completed

---
+++ typescript/sort/mergesort.ts
@@ -0,0 +1,48 @@
+
+
+function mergeSort(array : any[]) : any[] {
+
+    if (array.length <= 1) {
+        return array;
+    }
+
+    const middle = Math.floor(array.length / 2)
+    const leftHalf = array.slice(0,middle);
+    const rightHalf = array.slice(middle);
+
+    return merge(mergeSort(leftHalf), mergeSort(rightHalf))
+}
+
+
+
+function merge(left : any[], right : any[]) : any[] {
+
+    let result : any[] = [];
+    let leftIndex  = 0
+    let rightIndex = 0;
+
+    while (leftIndex < left.length && rightIndex < right.length) {
+        
+        if (left[leftIndex] < right[rightIndex]) {
+
+            result.push(left[leftIndex])
+            leftIndex ++;
+
+        } else {
+
+            result.push(right[rightIndex])
+            rightIndex++;
+
+        }
+ 
+    }
+
+    return result.concat(left.slice(leftIndex).concat(right.slice(rightIndex)))
+}
+
+let sortedNumberArrayy: number[] =
+    mergeSort([38, 27, 43, 10]);
+let sortedStringArray: string[] =
+    mergeSort(['JavaScript', 'GeeksforGeeks', 'TypeScript']);
+console.log(sortedNumberArrayy);
+console.log(sortedStringArray);

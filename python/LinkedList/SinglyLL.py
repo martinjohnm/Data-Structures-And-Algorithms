21323f8b59744280505230a36ec15321efb9b92a
martinjohnm <johnmmartin74@gmail.com>
2025-01-04 04:48:02 +0530

    completed insert at beginning of the LL

--- python/LinkedList/SinglyLL.py
+++ python/LinkedList/SinglyLL.py
@@ -6,3 +6,24 @@ class Node:
         
         self.data = data
         self.next = next
+
+
+def insert_at_beginning(head, data):
+    new_node = Node(data)
+    new_node.next = head
+    return new_node
+
+def traverse(head):
+    current = head
+    while current:
+        print(str(current.data) + " -> ", end=" ")
+        current = current.next
+    print("None")
+
+if __name__ == "__main__":
+    head = None
+    head = insert_at_beginning(head, 4)
+    head = insert_at_beginning(head, 3)
+    head = insert_at_beginning(head, 2)
+    head = insert_at_beginning(head, 1)
+    traverse(head)
